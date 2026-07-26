99edcf715c3dd90640ed3c58f73fc8ccb3b5cc74
RedAppr <jordanpfife@gmail.com>
2026-07-26 02:02:14 -0400

    feat: TUI prototype (#2)
    * feat: add TUI framework and basic model load page
    
    * fix: corrected connection status ping URL and updated connection icons to be termial compatible
    
    * fix: refactor connection status with dedicated update function and static enums
    
    * feat: add dedicated CSS style file with templates

--- src/lm_tui/main.py
+++ src/lm_tui/main.py
@@ -3,6 +3,7 @@ import asyncio
 from lm_tui.config import parse_arguments, AppConfig
 from lm_tui.scanner import scan_targets
 from lm_tui.api import fetch_available_models
+from lm_tui.tui import LMStudioApp
 
 
 async def execute_scan(config: AppConfig) -> None:
@@ -40,29 +41,20 @@ async def execute_scan(config: AppConfig) -> None:
 #         print("Scan cancelled by user.")
 #         sys.exit(2)
 
-async def main() -> None:
+def main() -> None:
     config: AppConfig | None
     err: str | None
 
-    config, err = parse_arguments()
+    config, err = parse_arguments(sys.argv[1:])
     if err or (config is None):
         print(f"Configuration error: {err}")
         sys.exit(1)
 
     ip: str = config.target.split('/')[0]
     port: int = config.port
-    print(f"Connecting to LM Studio Native API at {ip}:{port}...")
 
-    models, api_err = await fetch_available_models(ip, port)
-    if api_err:
-        print(f"Failure: {api_err}")
-        sys.exit(2)
-
-    if models is not None:
-        print()
-        print(f"Found {len(models)} available models:")
-        for model in models:
-            print(f"\t- {model.display_name} ({model.key})")
+    app = LMStudioApp(ip, port)
+    app.run()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
