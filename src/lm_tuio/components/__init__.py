6882ff32ce90ab9c313a6527030f370ecdb4489d
redappr <jordanpfife@gmail.com>
2026-08-01 18:22:56 -0400

    chore: remove deprecated modules for downloaded/loaded models components (moved to models.py)

--- src/lm_tuio/components/__init__.py
+++ src/lm_tuio/components/__init__.py
@@ -6,15 +6,6 @@ Exposes all custom widgets used across the primary application layout.
 from .action_log import ActionLog
 from .connection import ConnectionStatus
 from .context_pane import ContextPane
-from .downloaded_models import DownloadedModels
-from .loaded_models import LoadedModels
 from .title import Title
 
-__all__ = [
-    "ActionLog",
-    "ConnectionStatus",
-    "ContextPane",
-    "DownloadedModels",
-    "LoadedModels",
-    "Title"
-]
+__all__ = ["ActionLog", "ConnectionStatus", "ContextPane", "Title"]
